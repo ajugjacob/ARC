31be270dc3a7734b4c35566a829cbc41c6924bcb
Akhil Ravindran <imakhilravindran@gmail.com>
2020-04-05 17:35:42 +0530

    added footer.php and header.php

---
+++ footer.php
@@ -0,0 +1,64 @@
+
+
+  
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>Alumini Relation Cell</h3>
+            <p> Alumini Relation Cell works towards ensuring the mutual benefit of the alumini as well as the students. ARC helps the alumini in staying connected to their campus. ARC has been instrumental in organizing the Alumini Meets, Mentorship programme, regular guest lectures by distinguished alumini in the campus.</p>
+          </div>
+
+          <div class="col-lg-6 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="fa fa-angle-right"></i> <a href="#intro">Home</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#about">About us</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#gallery">Gallery</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#activities">Activities</a></li>
+             <!--  <li><i class="fa fa-angle-right"></i> <a href="#schedule">Alumini Meet</a></li> -->
+              <li><i class="fa fa-angle-right"></i> <a href="#contributions">Contribution</a></li>
+              <li><i class="fa fa-angle-right"></i> <a href="#speakers">Team</a></li>
+
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              College Of Engineering <br>
+              Chengannur, 689121<br>
+              Kerala <br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        <p>MADE WITH <strong style="color: red;">&hearts;</strong> BY DEVCOM</p>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Alumini Relation Cell</title>
+  <?php wp_head(); ?>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <!-- Uncomment below if you prefer to use a text logo -->
+        <h1><a href="#main">A<span>R</span>C</a></h1>
+        <!-- <a href="#intro" class="scrollto"><img src="alumni final.png" alt="" title=""></a> -->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li><a href="#intro">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#activities">Activities</a></li>
+          <!-- <li><a href="#schedule">Alumini Meet</a></li>  -->         
+          <li><a href="#contributions">Contributions</a></li>
+          <li><a href="#speakers">Team</a></li>
+          
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
